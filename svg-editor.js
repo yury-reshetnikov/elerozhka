f154ec1b20e0260695ae7e7e4853ad09227a5dcc
Yury Reshetnikov (reshu-big) <reshu@mail.ru>
2018-12-23 11:57:59 +0500

    _

---
+++ svg-editor.js
@@ -0,0 +1,12 @@
+(function(jQuery) {
+    if (typeof(jQuery.reshu) === "undefined") {
+        jQuery.reshu = {};
+    }
+
+    var plugin;
+
+    plugin = jQuery.reshu.svgeditor = function(svg) {
+	console.log(svg)
+    }
+
+})(jQuery);
