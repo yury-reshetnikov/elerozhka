aed1e3055a2a7da70405e9aaeebb9a3dffa342ac
Yury Reshetnikov (on office) <yura.reshetnikov@portal59.ru>
2018-12-25 18:00:52 +0500

    _

--- svg-editor.js
+++ svg-editor.js
@@ -4,9 +4,13 @@
     }
 
     var plugin;
+    var g = jQuery.reshu.pagegen;
 
     plugin = jQuery.reshu.svgeditor = function(svg) {
-	console.log(svg)
+	console.log(svg.children.length)
+	g(jQuery(svg), ['rect', {stroke:"none", fill:"rgb(114,159,207)", x:4710, y:2781, width:10399, height:7719}])
+        // <rect stroke="none" fill="rgb(114,159,207)" x="4710" y="2781" width="10399" height="7719"/>
+        // <text class="TextShape"><tspan class="TextParagraph" font-family="Liberation Sans, sans-serif" font-size="635px" font-weight="400"><tspan class="TextPosition" x="6122" y="5924"><tspan fill="rgb(0,0,0)" stroke="none">Это есть текстовое </tspan></tspan><tspan class="TextPosition" x="6122" y="6635"><tspan fill="rgb(0,0,0)" stroke="none">поле</tspan></tspan></tspan></text>
     }
 
 })(jQuery);
