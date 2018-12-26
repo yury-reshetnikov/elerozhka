e34dcdd70c5716c1b8f426ecb456b58698436f34
Yury Reshetnikov (on office) <yura.reshetnikov@portal59.ru>
2018-12-26 10:19:00 +0500

    _

--- svg-editor.js
+++ svg-editor.js
@@ -8,9 +8,18 @@
 
     plugin = jQuery.reshu.svgeditor = function(svg) {
 	console.log(svg.children.length)
-	g(jQuery(svg), ['rect', {stroke:"none", fill:"rgb(114,159,207)", x:4710, y:2781, width:10399, height:7719}])
+	// g(jQuery(svg), ['rect', {stroke:"none", fill:"rgb(114,159,207)", x:4710, y:2781, width:10399, height:7719}])
         // <rect stroke="none" fill="rgb(114,159,207)" x="4710" y="2781" width="10399" height="7719"/>
         // <text class="TextShape"><tspan class="TextParagraph" font-family="Liberation Sans, sans-serif" font-size="635px" font-weight="400"><tspan class="TextPosition" x="6122" y="5924"><tspan fill="rgb(0,0,0)" stroke="none">Это есть текстовое </tspan></tspan><tspan class="TextPosition" x="6122" y="6635"><tspan fill="rgb(0,0,0)" stroke="none">поле</tspan></tspan></tspan></text>
+	var xmlns = "http://www.w3.org/2000/svg"
+	var rect = document.createElementNS(xmlns, 'rect')
+	rect.setAttributeNS(null, 'fill', 'rgb(230,230,230)')
+	rect.setAttributeNS(null, 'x', 4710)
+	rect.setAttributeNS(null, 'y', 2781)
+	rect.setAttributeNS(null, 'width', 10399)
+	rect.setAttributeNS(null, 'height', 7719)
+	rect.setAttributeNS(null, 'fill-opacity', .9)
+	svg.appendChild(rect)
     }
 
 })(jQuery);
