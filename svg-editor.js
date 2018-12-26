1023f262749e01e81e87488c08aecf3cc5432766
Yury Reshetnikov (on office) <yura.reshetnikov@portal59.ru>
2018-12-26 13:33:55 +0500

    _

--- svg-editor.js
+++ svg-editor.js
@@ -4,22 +4,31 @@
     }
 
     var plugin;
-    var g = jQuery.reshu.pagegen;
+    // var g = jQuery.reshu.pagegen;
 
     plugin = jQuery.reshu.svgeditor = function(svg) {
 	console.log(svg.children.length)
-	// g(jQuery(svg), ['rect', {stroke:"none", fill:"rgb(114,159,207)", x:4710, y:2781, width:10399, height:7719}])
-        // <rect stroke="none" fill="rgb(114,159,207)" x="4710" y="2781" width="10399" height="7719"/>
         // <text class="TextShape"><tspan class="TextParagraph" font-family="Liberation Sans, sans-serif" font-size="635px" font-weight="400"><tspan class="TextPosition" x="6122" y="5924"><tspan fill="rgb(0,0,0)" stroke="none">Это есть текстовое </tspan></tspan><tspan class="TextPosition" x="6122" y="6635"><tspan fill="rgb(0,0,0)" stroke="none">поле</tspan></tspan></tspan></text>
+	svg.width.baseVal.value = svg.clientWidth
+	svg.viewBox.baseVal.width = svg.width.baseVal.valueInSpecifiedUnits * 100
+	if(svg.height.baseVal.value < svg.clientHeight) {
+	    svg.height.baseVal.value = svg.clientHeight
+	    svg.viewBox.baseVal.height = svg.height.baseVal.valueInSpecifiedUnits * 100
+	}
+	/*
 	var xmlns = "http://www.w3.org/2000/svg"
 	var rect = document.createElementNS(xmlns, 'rect')
-	rect.setAttributeNS(null, 'fill', 'rgb(230,230,230)')
-	rect.setAttributeNS(null, 'x', 4710)
-	rect.setAttributeNS(null, 'y', 2781)
-	rect.setAttributeNS(null, 'width', 10399)
-	rect.setAttributeNS(null, 'height', 7719)
-	rect.setAttributeNS(null, 'fill-opacity', .9)
+	rect.setAttribute('fill', 'rgb(230,230,230)')
+	rect.setAttribute('x', 20000)
+	rect.setAttribute('y', 2781)
+	rect.setAttribute('width', 10399)
+	rect.setAttribute('height', 7719)
+	rect.setAttribute('fill-opacity', .9)
 	svg.appendChild(rect)
+	window.rect = rect
+	*/
+	window.rect = svggen(svg, ['rect', {fill:"rgb(230,230,230)", x:20000, y:2781, width:10399, height:7719, 'fill-opacity':.9}])
+	window.svg = svg
     }
 
 })(jQuery);
---
+++ svggen.js
@@ -0,0 +1,157 @@
+(function(){
+    var xmlns = "http://www.w3.org/2000/svg"
+    var plugin
+
+    var append_child = function(node, child) { node.appendChild(child) }
+
+    var p_run = function(p, args) {
+	var list1 = p.list
+	var list2 = p.list = []
+	plugin.run(p, args, 0)
+	p.list = list1.concat(list2)
+	return list2
+    }
+
+    var append = function(obj, method) {
+	var p = function() { return p_run(p, arguments) }
+	if(!method) method = append_child
+	p.obj = obj
+	p.list = []
+	p.add = function(c) {
+	    method(this.obj, c)
+	    this.list.push(c)
+	    return c
+	}
+	p.tag = function() { return p.add(plugin.tag(arguments)) }
+	return p
+    }
+
+    // +++ prepend
+
+    var merge = function() {
+	var p = function() { return p_run(p, arguments) }
+	p.list = []
+	p.add = function(c) { this.list.push(c); return c; }
+	p.tag = function() { return p.add(plugin.tag(arguments)) }
+	return p;
+    };
+
+    plugin = window.svggen = function(a0) {
+	if(arguments.length) {
+	    if(a0 instanceof Element) return plugin.run(append(a0), arguments, 1)
+	    else if(a0 instanceof Array && (a0.length == 2 || a0.length == 3) && a0[0] == '#')
+		return plugin.run(append(plugin._id(a0)), arguments, 1)
+	    else if(a0 instanceof Array) return plugin.run(merge(), arguments, 0)
+	    else if(a0 instanceof Function && a0.list && a0.add)
+		return plugin.run(a0, arguments, 1)
+	    else return plugin.tag(arguments)
+	}
+    }
+
+    plugin.any = function(t) { return plugin.run(t, arguments, 1) }
+
+    plugin.run = function(p, a, i) {
+	for(; i < a.length; i++) {
+	    var def = a[i]
+	    if(typeof def === "undefined") ;
+	    else if(def instanceof Element) p.add(def)
+	    else if(def instanceof Array) {
+		if(def.length) {
+		    var cmd
+		    if(typeof def[0] !== 'string') plugin.run(p, def, 0)
+		    else if(cmd = plugin.cmds[def[0]]) cmd(p, def)
+		    else p.add(plugin.tag(def))
+		}
+	    }
+	    else if(def instanceof Function) def(p)
+	    // +++ else p.add(jQuery(document.createTextNode(def)));
+	    else throw new Error('Элемент не поддерживается: ' + def)
+	}
+	return p.list
+    }
+
+    plugin.attr = function(tag, attr) {
+	for(var k in attr) {
+	    if(k == 'style') {
+		// +++ for(var s in attr['style']) tag.css(s, attr['style'][s]);
+		throw new Error('Элемент не поддерживается: ' + attr['style'])
+	    }
+	    // +++ else if(k.substr(0, 2) == 'on' && attr[k] instanceof Function) tag.bind(k.substr(2), attr[k]);
+	    else tag.setAttribute(k, attr[k]);
+	}
+    }
+
+    plugin.tag = function(def) {
+	var tag = document.createElementNS(xmlns, def[0])
+	if(def.length > 1) {
+	    var i = 1
+	    if(def[1] instanceof Object && !(
+		    def[1] instanceof Array || def[1] instanceof Function)) {
+		plugin.attr(tag, def[1])
+		++i
+	    }
+	    plugin.run(append(tag), def, i)
+	}
+	return tag
+    }
+
+    plugin.cmds = {
+
+	noop: function(p, def) { ; },
+	ce: function(p, def) { plugin.run(p, def, 1); },
+	run: function(p, def) { plugin.run(p, def[1], def.length > 2 ? def[2] : 0); },
+
+	join: function(p, def) {
+	    if(def.length > 2) {
+		plugin.any(p, def[2])
+		for(var i = 3; i < def.length; i++) {
+		    plugin.any(p, def[1], def[i])
+		}
+	    }
+	},
+
+	map: function(p, def) {
+	    var arr = def[1], cb = def[2], sep = def[3]
+	    if(!arr || !arr.length) return
+	    if(sep) {
+		cb(p, arr[0])
+		for(var i = 1; i < arr.length; i++) { p(sep); cb(p, arr[i]); }
+	    }
+	    else for(var i = 0; i < arr.length; i++) cb(p, arr[i])
+	},
+
+	phpmap: function(p, def) {
+	    if(def[1] instanceof Array) {
+		for(var i = 0; i < def[1].length; i++) def[2](p, def[1][i])
+	    }
+	    else { for(var k in def[1]) def[2](p, def[1][k]); }
+	},
+
+	objmap: function(p, def) { if(def[1]) for(var k in def[1]) def[2](p, def[1][k], k); },
+
+    }
+
+    plugin.id = function(id, cr) {
+	var r = document.getElementById(id);
+	if(r) return r
+	else {
+	    if(arguments.length == 1) throw new Error('Не найден элемент #' + id)
+	    else if(cr === false) return
+	    else {
+		r = cr()
+		if(r === false) return
+		else if(r instanceof Element) return r
+		else {
+		    var r = document.getElementById(id)
+		    if(r) return r
+		    else throw new Error('Не найден элемент #' + id)
+		}
+	    }
+	}
+    }
+    plugin._id = function(def) {
+	if(def.length < 2) throw new Error('Слишком мало аргументов для \'#\'')
+	else if(def.length == 2) return plugin.id(def[1])
+	else return plugin.id(def[1], def[2])
+    };
+})()
