79b0592bd43e5dedf17e9f893b4baadfbcdf615b
Yury Reshetnikov (on office) <yura.reshetnikov@portal59.ru>
2018-12-29 15:41:04 +0500

    _

--- svg-editor.js
+++ svg-editor.js
@@ -1,12 +1,35 @@
 (function() {
-    var text_height = 400, text_line = 100
-    var edgroup, text, selection, selno = false, bbox
+    var text_left, text_height = 400, text_line = 100
+    var edgroup, text, selection, selno = false, bbox, root, rootsel = []
 
     var plugin
 
+    var text_node = function(item, text_top) {
+	var t = svggen(text, ['tspan', {
+	    'font-size': text_height, x: text_left, y: text_top
+	}, item.nodeName])[0]
+	t.editor_data = item
+	if(item.id) svggen(t, ['tspan', '#'+item.id])
+	if(item.nodeName == 'path') {
+	    svggen(t, ['tspan', ' '+item.attributes['d'].value])
+	}
+    }
+
+    var text_children = function(root, text_top) {
+	for(let item of root.children) {
+	    if(item.nodeName == 'script') ;
+	    else if(item.nodeName == 'g' && item.id == 'svg-editor-group') ;
+	    else {
+		text_top += text_height
+		text_node(item, text_top)
+		if(text_top >= svg.viewBox.baseVal.height) break
+	    }
+	}
+    }
+
     var create = function(svg) {
 	window.svg = svg
-	var old_width = svg.viewBox.baseVal.width
+	text_left = svg.viewBox.baseVal.width
 	if(svg.width.baseVal.value < svg.clientWidth) {
 	    svg.width.baseVal.value = svg.clientWidth
 	    svg.viewBox.baseVal.width = svg.width.baseVal.valueInSpecifiedUnits * 100
@@ -22,33 +45,17 @@
 	edgroup = svggen(svg, ['g', {id: 'svg-editor-group'}])[0]
 	svggen(edgroup, ['rect', {
 	    fill:"rgb(250,250,250)", 'fill-opacity':.8,
-	    x:old_width, y:0,
-	    width: svg.viewBox.baseVal.width - old_width, height: svg.viewBox.baseVal.height
+	    x:text_left, y:0,
+	    width: svg.viewBox.baseVal.width - text_left, height: svg.viewBox.baseVal.height
 	}])
 	selection = svggen(edgroup, ['rect', {
 	    fill:"rgb(100,255,100)", 'fill-opacity':.9,
-	    x: old_width, y: text_line,
-	    width: svg.viewBox.baseVal.width - old_width, height: text_height,
+	    x: text_left, y: text_line,
+	    width: svg.viewBox.baseVal.width - text_left, height: text_height,
 	    style: { display: 'none' }
 	}])[0]
 	text = svggen(edgroup, ['text', {'font-size': text_height}])[0]
-	var text_top = 0
-	for(let item of svg.children) {
-	    if(item.nodeName == 'script') ;
-	    else if(item.nodeName == 'g' && item.id == 'svg-editor-group') ;
-	    else {
-		text_top += text_height
-		var t = svggen(text, ['tspan', {
-		    'font-size': text_height, x: old_width, y: text_top
-		}, item.nodeName])[0]
-		t.editor_data = item
-		if(item.id) svggen(t, ['tspan', '#'+item.id])
-		if(item.nodeName == 'path') {
-		    svggen(t, ['tspan', ' '+item.attributes['d'].value])
-		}
-		if(text_top >= svg.viewBox.baseVal.height) break
-	    }
-	}
+	text_children(svg, 0)
     }
 
     var show_selection = function() {
@@ -56,10 +63,13 @@
 	selection.style.display = ''
 	if(bbox) bbox.remove()
 	var p = text.children[selno].editor_data.getBBox()
+	if(p.width < 100) { p.x -= (100 - p.width) / 2; p.width = 100; }
+	if(p.height < 100) { p.y -= (100 - p.height) / 2; p.height = 100; }
 	bbox = svggen(edgroup, ['rect', {
 	    fill:"rgb(250,250,250)", 'fill-opacity':.8,
 	    stroke: 'black', 'stroke-dasharray': '1 3',
-	    x: p.x, y: p.y, width: p.width, height: p.height
+	    x: p.x - p.width / 10, y: p.y - p.height / 10,
+	    width: p.width * 1.2, height: p.height * 1.2
 	}])[0]
     }
 
@@ -132,7 +142,23 @@
 	else if(e.key == 'Enter') {
 	    if(selno === false) ;
 	    else {
-		console.log(text.children[selno].editor_data.getBBox())
+		if(root == text.children[selno].editor_data) {
+		    root = text.children[selno].editor_data.parentNode
+		    selno = rootsel.pop()
+		}
+		else {
+		    root = text.children[selno].editor_data
+		    rootsel.push(selno)
+		    selno = 0
+		}
+		while(text.firstChild) text.removeChild(text.firstChild)
+		if(root.nodeName == 'svg')
+		    text_children(root, 0)
+		else {
+		    text_node(root, text_height)
+		    text_children(root, text_height)
+		}
+		show_selection()
 	    }
 	}
 	// else console.log(e)
