f4fae1b49c2bd341df991b4a36df84e21f82152c
Yury Reshetnikov (reshu-big) <reshu@mail.ru>
2019-03-09 13:01:09 +0500

    _

--- svg-animate.js
+++ svg-animate.js
@@ -13,6 +13,34 @@ function Animate3() {
 	    }
 	}
     }
+    function Path(id, pattern_from, pattern_to, time_from, time_to) {
+	this.draw = function(t) {
+	    if(t > time_to) return false
+	    else if(t < time_from) return true
+	    else {
+		var d = []
+		var d0 = document.getElementById(pattern_from).attributes.d.value.split(/\s+/)
+		var d1 = document.getElementById(pattern_to).attributes.d.value.split(/\s+/)
+		while(d0.length && d1.length) {
+		    var v0 = d0.shift()
+		    var v1 = d1.shift()
+		    var m0 = v0.match(/(\d+),(\d+)/)
+		    var m1 = v1.match(/(\d+),(\d+)/)
+		    if(m0 && m1) {
+			var x0 = parseInt(m0[1])
+			var y0 = parseInt(m0[2])
+			var x1 = parseInt(m1[1])
+			var y1 = parseInt(m1[2])
+			var k = (t - time_from) / (time_to - time_from)
+			d.push(''+(x0 + (x1 - x0) * k)+','+(y0 + (y1 - y0) * k))
+		    }
+		    else d.push(v1)
+		}
+		document.getElementById(id).attributes.d.value = d.join(' ')
+		return true
+	    }
+	}
+    }
     // this.actions = []
     var actions = []
     var finalization = []
@@ -36,6 +64,9 @@ function Animate3() {
     }
     this.rotate = function(id, angle_from, angle_to, time_from, time_to) {
 	actions.push(new Rotate(id, angle_from, angle_to, time_from, time_to))
+    }
+    this.path = function(id, pattern_from, pattern_to, time_from, time_to) {
+	actions.push(new Path(id, pattern_from, pattern_to, time_from, time_to))
     }
     this.finish = function(cb) { finalization.push(cb) }
 }
