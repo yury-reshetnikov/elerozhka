98e70a40c4dc8ae3d3f5ffa14e26d5c3e7a0b110
Yury Reshetnikov (on office) <yura.reshetnikov@portal59.ru>
2019-03-10 16:05:06 +0500

    -

---
+++ image.js
@@ -0,0 +1,176 @@
+var star = 1
+function id_display(id, display) {
+    document.getElementById(id).style.display = display
+}
+function star_display(state, display) { id_display('star'+star+state, display) }
+function star_eye(show) {
+    if(show) id_display('eye_open', '')
+    else {
+	id_display('eye_open', 'none')
+	id_display('eye_close', 'none')
+    }
+}
+function star_eye2(show) {
+    if(show) id_display('eye2_open', '')
+    else {
+	id_display('eye2_open', 'none')
+    }
+}
+function set_star(n) {
+    if(star != n) {
+	star_display('on', 'none')
+	star_display('off', '')
+	star = n
+	star_display('off', 'none')
+	star_display('on', '')
+	star_eye(star == 1)
+	star_eye2(star == 2)
+    }
+}
+function close_eye() {
+    document.getElementById('eye_open').style.display = 'none'
+    document.getElementById('eye_close').style.display = ''
+}
+function open_eye() {
+    document.getElementById('eye_open').style.display = ''
+    document.getElementById('eye_close').style.display = 'none'
+}
+function open_eye2(eye) {
+	    ['eye2_open','eye2_right','eye2_right2','eye2_right3',
+	     'eye2_almost_open_1','eye2_almost_open_2','eye2_almost_close_1',"eye2_almost_close_2","eye2_close"].forEach(function(item) {
+		id_display(item, eye == item ? '' : 'none')
+	    })
+}
+function animate(timeout,finish,count,...steps) {
+    var pos = 0
+    var call = function iterate() {
+	steps[pos]()
+	if(!--count) finish()
+	else {
+	    if(++pos >= steps.length) pos = 0
+	    setTimeout(iterate, timeout)
+	}
+    }
+    call()
+}
+function animate2(timeout,finish,count,steps) {
+    var pos = 0
+    count = count * steps.length + 1
+    var call = function iterate() {
+	var steps_pos = steps[pos]
+	var show = steps_pos instanceof Array ? function(item) {
+	    var d = 'none'
+	    steps_pos.forEach(function(sp) {
+		if(sp == item) d = ''
+	    })
+	    id_display(item, d)
+	} : function(item) {
+	    id_display(item, item == steps_pos ? '' : 'none')
+	}
+	steps.forEach(function(item) {
+	    if(item instanceof Array) item.forEach(show)
+	    else show(item)
+	})
+	if(!--count) finish()
+	else {
+	    if(++pos >= steps.length) pos = 0
+	    setTimeout(iterate, timeout)
+	}
+    }
+    call()
+}
+var wink_started = false
+function wink() {
+    if(wink_started || sneeze_started) return
+    if(star == 1) {
+	wink_started = true
+	animate(300, function() { wink_started = false }, 5,
+		function() { open_eye() },
+		function() { close_eye() })
+    }
+    else if(star == 2) {
+	wink_started = true
+	animate(100, function() { wink_started = false }, 1+1*16,
+		function() { open_eye2('eye2_open') },
+		function() { open_eye2('eye2_right') },
+		function() { open_eye2('eye2_right2') },
+		function() { open_eye2('eye2_right3') },
+		function() { open_eye2('eye2_right2') },
+		function() { open_eye2('eye2_right') },
+		function() { open_eye2('eye2_open') },
+		function() { open_eye2('eye2_almost_open_1') },
+		function() { open_eye2('eye2_almost_open_2') },
+		function() { open_eye2('eye2_almost_close_1') },
+		function() { open_eye2('eye2_almost_close_2') },
+		function() { open_eye2('eye2_close') },
+		function() { open_eye2('eye2_almost_close_2') },
+		function() { open_eye2('eye2_almost_close_1') },
+		function() { open_eye2('eye2_almost_open_2') },
+		function() { open_eye2('eye2_almost_open_1') },
+	       )
+    }
+}
+
+var stomp_started = false
+function stomp() {
+    if(stomp_started) return
+    stomp_started = true
+    animate2(200, function() { stomp_started = false }, 2, [
+		'raised_hoof_1', 'raised_hoof_2', 'raised_hoof_3', 'raised_hoof_2',
+		['raised_hoof_1', 'hit3'], ['raised_hoof_1', 'hit2'], ['raised_hoof_1', 'hit1'],
+    ]);
+}
+
+var wag_started = false
+function wag() {
+    if(wag_started || sneeze_started) return
+    wag_started = true
+    animate2(150, function() { wag_started = false }, 3, [
+                'tail_3', 'tail_2', 'tail_1', 'tail_2'
+              ])
+}
+
+var smile_started = false
+function smile() {
+    if(smile_started) return
+    smile_started = true
+    animate2(150, function() { smile_started = false }, 2, [
+                'jowl_2', 'jowl_1'
+              ])
+}
+
+var sneeze_started = false
+function sneeze() {
+    if(sneeze_started || wink_started || wag_started) return
+    sneeze_started = true
+    animate2(300, function() { sneeze_started = false }, 1, [
+                ['head','tail_3'], ['head_up1','tail_3'], ['head_up2','tail_3'],
+		['head_up1_eye','tail_3'], ['head_up','tail_3'],
+		['head_down', 'tail_sneeze'],
+                ['head','tail_3','germs2'], ['head_up1','tail_3'], ['head_up2','tail_3'],
+		['head_up1_eye','tail_3'], ['head_up','tail_3'],
+		['head_down', 'tail_sneeze'],
+		['head','tail_3','germs2']
+                ])
+}
+
+var eating_started = false
+function eating() {
+    if(eating_started || sneeze_started || wink_started) return
+    eating_started = true
+    var a = new Animate3
+    a.rotate('head_without_neck', 0, -50, 0, 1000)
+    a.rotate('head_without_neck', -50, 0, 1300, 2300)
+    a.path_rotate('contour', 'contour_t0', 10807, 12015, [0,2], 0, -50, 0, 1000)
+    a.path_rotate('contour', 'contour_t0', 10807, 12015, [0,2], -50, 0, 1300, 2300)
+    a.path('back', 'back_t0', 'back_t1', 0, 1000)
+    a.path('back', 'back_t1', 'back_t0', 1300, 2300)
+    a.finish(function() { eating_started = false })
+    a.start()
+}
+
+function rotate_head(r) {
+    document.getElementById('head_without_neck').setAttribute('transform', 'rotate('+r+')')
+}
+
+window.onkeyup = svgeditor
