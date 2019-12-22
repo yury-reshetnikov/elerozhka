beb322fb56c5978163a36570f96e70d87a2e19a1
Yury Reshetnikov (on office) <yura.reshetnikov@portal59.ru>
2019-12-22 14:55:06 +0500

    _

--- snake.js
+++ snake.js
@@ -7,11 +7,11 @@ function rotate_sin_cos(m, sin, cos) {
     m.f = 0
 }
 
-function move1(m1, m2, a, d) {
+function move1(m1, m2, a, d, sign_y) {
     m1.e = d - 2 * a
-    m1.f = d
+    m1.f = d * sign_y
     m2.e = d - 2 * a
-    m2.f = d
+    m2.f = d * sign_y
 }
 
 function move2(m1, m2, a, d, sign) {
@@ -53,11 +53,14 @@ function start() {
      limit.y.bottom += y
    }
    let rotate_start = false
-   let rotate_left
+   let rotate_left, rotate_right
    let rotate_tail = false
    // console.log(limit)
    // console.log(snake_head_rotate.transform)
    // console.log(snake_head_rotate.transform.baseVal[0])
+   // смена стартовой позиции при необходимости
+   //   snake.transform.baseVal[0].matrix.e = 0
+   //   snake.transform.baseVal[0].matrix.f = -10000
 /**
     { // полный поворот вверх
 	rotate_sin_cos(snake_head_rotate.transform.baseVal[0].matrix, -1, 0)
@@ -80,27 +83,13 @@ function start() {
 	       rotate_left = true
 	       rotate_start = speed.x ? snake.transform.baseVal[0].matrix.e :
 		   snake.transform.baseVal[0].matrix.f
-	       console.log('rotate_start:', rotate_start)
-            /*
-            if (speed.x) {
-                speed.y = -speed.x
-                speed.x = 0
-            }
-            else {
-                speed.x = speed.y
-                speed.y = 0
-            }
-            */
+	       console.log('left rotate_start:', rotate_start)
 	   }
 	   else if(e.key == 'ArrowRight') {
-            if(speed.x) {
-                speed.y = speed.x
-                speed.x = 0
-            }
-            else {
-                speed.x = -speed.y
-                speed.y = 0
-            }
+               rotate_right = true
+               rotate_start = speed.x ? snake.transform.baseVal[0].matrix.e :
+		   snake.transform.baseVal[0].matrix.f
+	       console.log('right rotate_start:', rotate_start)
 	   }
 	   else if(other_keyup) other_keyup(e)
    }
@@ -134,10 +123,11 @@ function start() {
 		      move1(snake_head_shift.transform.baseVal[0].matrix,
 			    snake_body_1.transform.baseVal[0].matrix,
 			    speed.x > 0 ? 0 : snake_body_length,
-			    (leg - snake_body_length) * sign)
+			    (leg - snake_body_length) * sign,
+                            rotate_left ? 1 : -1)
 		  }
 		  else {
-                      speed.y = -speed.x
+                      speed.y = rotate_left ? -speed.x : speed.x
                       speed.x = 0
 		      rotate_start = false
 		      rotate_tail = y
@@ -177,18 +167,22 @@ function start() {
       if(rotate_tail !== false) {
 	  if(speed.y) {
               let sign = speed.y < 0 ? 1 : -1
-	      // let leg = snake_tail_length - (rotate_tail - y)
 	      let leg = (rotate_tail - y) * sign
 	      if(leg <= snake_tail_length) {
 		  let sin = leg / snake_tail_length
+	          if(rotate_right) {
+                      sign = -sign
+                      sin = -sin
+	          }
 		  let angle = Math.asin(sin)
 		  let cos = Math.cos(angle)
-		  if(rotate_left) sin = -sin
+		  sin = -sin
 		  rotate_sin_cos(snake_tail.transform.baseVal[0].matrix, sin * sign, cos * sign)
 	      }
 	      else {
 		  rotate_sin_cos(snake_tail.transform.baseVal[0].matrix, -1 * sign, 0)
 		  rotate_tail = false
+		  console.log('rotate finished')
 	      }
 	  }
 	  else {
@@ -204,9 +198,11 @@ function start() {
 	      else {
 		  rotate_sin_cos(snake_tail.transform.baseVal[0].matrix, 0, -1 * sign)
 		  rotate_tail = false
+		  console.log('rotate finished')
 	      }
 	  }
       }
+      /*
       if (x - delta_rotate_x >= limit.x.right || x - delta_rotate_x <= limit.x.left ||
           y - delta_rotate_y >= limit.y.bottom || y - delta_rotate_y <= limit.y.top) {
           console.log({x:x, y:y, limit:limit,
@@ -214,6 +210,7 @@ function start() {
 		       speed:speed})
         return
       }
+      */
       snake.transform.baseVal[0].matrix.e = x
       snake.transform.baseVal[0].matrix.f = y
       prev = time
