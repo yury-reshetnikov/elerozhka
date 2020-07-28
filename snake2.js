e0dfdb63969010f8b84ff24c1cea26fcfbcf6a9c
Yury Reshetnikov <reshu@mail.ru>
2020-07-28 17:56:12 +0500

    _

--- snake2.js
+++ snake2.js
@@ -578,13 +578,16 @@ function start() {
 	   }
 	}
 	else {
+	    let distance_border = snake_body_length_half
+	    if(delta_rotate_x || delta_rotate_y)
+		distance_border = Math.round(distance_border * 1.2)
 	    let nearest_mouse
 	    let nearest_distance
 	    mice.some(function(mouse, mouse_index){
 		let mx = mouse.transform.baseVal[0].matrix.e + mouse_delta_x - snake_delta_x
 		let my = mouse.transform.baseVal[0].matrix.f + mouse_delta_y - snake_delta_y
 		let mouse_distance = Math.sqrt(Math.pow(mx - dx, 2) + Math.pow(my - dy, 2))
-		if(mouse_distance < snake_body_length / 2) {
+		if(mouse_distance < distance_border) {
 		    eating = { x: mx, y: my, index: mouse_index }
 		    return true
 		}
