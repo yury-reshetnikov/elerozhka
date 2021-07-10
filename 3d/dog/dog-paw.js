c2763f3754002affc6213678c7674ad2059b3c25
Yury Reshetnikov <reshu@mail.ru>
2021-07-10 18:02:48 +0500

    _

--- 3d/dog/dog-paw.js
+++ 3d/dog/dog-paw.js
@@ -69,6 +69,7 @@ function Paw(g, foot_points) {
     }
     let current_angles = [0, 0, 0, 0, 0, 0, 0, 0, 0]
     this.current_angles = function() { return current_angles }
+    this.get_points = function() { return points }
     this.rotate = function() {
 	current_angles = [...arguments]
 	let angle = new THREE.Quaternion() // +++ TODO учесть текущий поворот корпуса
