8909652d429b099d20e16fc10e6a1f62b7fac43e
Yury Reshetnikov <reshu@mail.ru>
2022-07-18 20:30:07 +0500

    _

--- 3d/dog/dog-paw.js
+++ 3d/dog/dog-paw.js
@@ -102,4 +102,62 @@ function Paw(g, foot_points) {
 	}
 	g.attributes.position.needsUpdate = true
     }
+    this.qrotate = function() {
+	current_angles = [...arguments]
+	let angle = new THREE.Quaternion() // +++ TODO учесть текущий поворот корпуса
+	let rc = (new THREE.Vector3()).fromArray(segments[2].rc)
+	for(let s = 2; s >= 0; --s) {
+	    angle = angle.multiply(arguments[2-s])
+	    let k = 0
+	    segments[s].pl.forEach(function(n) {
+		points[n].forEach(function(p,i) {
+		    let v = (new THREE.Vector3()).fromArray(bases[s],
+			(k++)*3).applyQuaternion(angle)
+		    let d = (new THREE.Vector3()).addVectors(rc, v)
+		    g.attributes.position.setXYZ(p, d.x, d.y, d.z)
+		})
+	    })
+	    if(s) {
+		let v = (new THREE.Vector3()).fromArray(bases[s], k*3).applyQuaternion(angle)
+		rc = (new THREE.Vector3()).addVectors(rc, v)
+	    }
+	}
+	g.attributes.position.needsUpdate = true
+    }
+    this.zy_rotate = function(y,z1,z2,z3) {
+	current_angles = []
+	let angle = new THREE.Quaternion() // +++ TODO учесть текущий поворот корпуса
+	function amul(a,z) {
+	    return a.clone().multiply((new THREE.Quaternion()).setFromEuler(
+		new THREE.Euler(0,0, THREE.MathUtils.degToRad(z), 'XYZ')
+	    ))
+	}
+	let a1 = amul(angle, z1)
+	let a2 = amul(a1, z2)
+	let a3 = amul(a2, z3)
+	let ay = (new THREE.Quaternion()).setFromEuler(
+	    new THREE.Euler(0, THREE.MathUtils.degToRad(y), 0, 'XYZ')
+	)
+	let angles = [a1.clone().multiply(ay),
+		      a2.clone().multiply(ay),
+		      a3.clone().multiply(ay)]
+	let rc = (new THREE.Vector3()).fromArray(segments[2].rc)
+	for(let s = 2; s >= 0; --s) {
+	    angle = angles[2-s]
+	    let k = 0
+	    segments[s].pl.forEach(function(n) {
+		points[n].forEach(function(p,i) {
+		    let v = (new THREE.Vector3()).fromArray(bases[s],
+			(k++)*3).applyQuaternion(angle)
+		    let d = (new THREE.Vector3()).addVectors(rc, v)
+		    g.attributes.position.setXYZ(p, d.x, d.y, d.z)
+		})
+	    })
+	    if(s) {
+		let v = (new THREE.Vector3()).fromArray(bases[s], k*3).applyQuaternion(angle)
+		rc = (new THREE.Vector3()).addVectors(rc, v)
+	    }
+	}
+	g.attributes.position.needsUpdate = true
+    }
 }
